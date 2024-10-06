825e83953ada240a7eeb26031e673a2c249bbaa5
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-10-06 16:12:36 -0500

    Ampliación de la clase Quiz para incluir un sistema de puntuacion

--- question/Quiz.py
+++ question/Quiz.py
@@ -2,6 +2,8 @@ class Quiz:
     def __init__(self):
         self.questions = []
         self.current_question_index = 0
+        self.correct_answer = 0
+        self.incorrect_answer = 0
     
     def add_question(self, question):
         self.questions.append(question)
@@ -12,3 +14,11 @@ class Quiz:
             self.current_question_index += 1
             return question
         return None
+    
+    def answer_question(self, question, answer):
+        if question.is_correct(answer):
+            self.correct_answer += 1
+            return True
+        else:
+            self.incorrect_answer += 1
+            return False
