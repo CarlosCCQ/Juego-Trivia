24b973674361a126d39a78a9a65dc769e70bfbdb
CarlosCCQ <jhan.ccanto.q@uni.pe>
2024-10-06 20:00:51 -0500

    Uso de excepciones y refactorizacion

--- testquestion/test_quiz_scoring.py
+++ testquestion/test_quiz_scoring.py
@@ -8,3 +8,10 @@ def test_quiz_scoring():
     quiz.add_question(question)
     assert quiz.answer_question(question,"4") == True
     assert quiz.correct_answer == 1
+
+def test_quiz_scoring_incorrect_answer():
+    quiz = Quiz()
+    question = Question("What is 2 + 2?",["1","2","3","4"],"4","fácil")
+    quiz.add_question(question)
+    assert not quiz.answer_question(question,"2")
+    assert quiz.incorrect_answer == 1
